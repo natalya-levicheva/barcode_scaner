60b367fa1098edbb5701a8b709474a623179ac34
Левичева Наталья <NDLevicheva@greenatom.ru>
2021-10-03 21:22:27 +0300

    fix animation, change color of support bar

--- app/src/main/java/com/college/qrscaner/MainActivity.java
+++ app/src/main/java/com/college/qrscaner/MainActivity.java
@@ -1,8 +1,11 @@
 package com.college.qrscaner;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
@@ -14,18 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+        this.getWindow().setStatusBarColor(ContextCompat.getColor(this,R.color.blue_deep));
         ImageView imageBarcode = findViewById(R.id.image_barcode);
-        Animation animationBarcodeToMax1 = AnimationUtils.loadAnimation(this, R.anim.scale_to_max);
-        Animation animationBarcodeToMin1 = AnimationUtils.loadAnimation(this, R.anim.scale_to_min);
-        Animation animationBarcodeToMax2 = AnimationUtils.loadAnimation(this, R.anim.scale_to_max);
-        Animation animationBarcodeToMin2 = AnimationUtils.loadAnimation(this, R.anim.scale_to_min);
-        animationBarcodeToMax1.setStartOffset(1);
-        animationBarcodeToMax2.setStartOffset(2000);
-        animationBarcodeToMin1.setStartOffset(1000);
-        animationBarcodeToMin2.setStartOffset(3000);
-        imageBarcode.startAnimation(animationBarcodeToMax1);
-        imageBarcode.startAnimation(animationBarcodeToMin1);
-        //imageBarcode.startAnimation(animationBarcodeToMax2);
-        //imageBarcode.startAnimation(animationBarcodeToMin2);
+
+        Animation animationBarcodeToMax = AnimationUtils.loadAnimation(this, R.anim.scale_to_max);
+        animationBarcodeToMax.setStartOffset(0);
+
+        imageBarcode.startAnimation(animationBarcodeToMax);
+
     }
 }
